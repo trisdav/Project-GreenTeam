a001023fe4765587e1ab8b5034cc40e89912285a
LydiaMcGovern <36086812+LydiaMcGovern@users.noreply.github.com>
2018-02-27 19:03:47 -0600

    Add files via upload

---
+++ src/action.java
@@ -0,0 +1,18 @@
+/**
+ * This is a global set of actions for the Controller to perform
+ * This may not be a complete set of actions
+ * @author Lydia McGovern
+ *
+ */
+public enum action {
+	COMPOSE,
+	SEND,
+	REPLY,
+	TRASH,
+	CREATE_USER,
+	CREATE_ACCOUNT,
+	DELETE_USER,
+	DELETE_ACCOUNT,
+	READ_EMAIL
+
+}
