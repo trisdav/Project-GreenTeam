c76ebfa44eb06ab3b7802b522925d1e00928fdde
HeyubCS <tridav95@gmail.com>
2018-04-13 15:42:12 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String args[]) {
-
-		View gui = new View();		
-		Model simpleEmailSystem = new Model();
-		Controller ctrlr = new Controller(gui, simpleEmailSystem);
-		gui.run(ctrlr);
-	}
-}
